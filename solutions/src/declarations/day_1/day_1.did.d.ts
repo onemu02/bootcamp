17a8175f419f94d0e9a89f2d2ca4304e7234d6fc
Nego <negogamechannel@gmail.com>
2022-03-06 13:50:01 +0900

    rebuild

--- solutions/src/declarations/day_1/day_1.did.d.ts
+++ solutions/src/declarations/day_1/day_1.did.d.ts
@@ -7,6 +7,11 @@ export interface _SERVICE {
   'greet' : (arg_0: string) => Promise<string>,
   'increment_counter' : (arg_0: bigint) => Promise<bigint>,
   'is_even' : (arg_0: bigint) => Promise<boolean>,
+  'maximum' : (arg_0: Array<bigint>) => Promise<bigint>,
+  'remove_from_array' : (arg_0: Array<bigint>, arg_1: bigint) => Promise<
+      Array<bigint>
+    >,
+  'selection_sort' : (arg_0: Array<bigint>) => Promise<Array<bigint>>,
   'square' : (arg_0: bigint) => Promise<bigint>,
   'sum_of_array' : (arg_0: Array<bigint>) => Promise<bigint>,
 }
--- solutions/src/declarations/day_1/day_1.did.js
+++ solutions/src/declarations/day_1/day_1.did.js
@@ -7,6 +7,13 @@ export const idlFactory = ({ IDL }) => {
     'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
     'increment_counter' : IDL.Func([IDL.Nat], [IDL.Nat], []),
     'is_even' : IDL.Func([IDL.Nat], [IDL.Bool], []),
+    'maximum' : IDL.Func([IDL.Vec(IDL.Nat)], [IDL.Nat], []),
+    'remove_from_array' : IDL.Func(
+        [IDL.Vec(IDL.Nat), IDL.Nat],
+        [IDL.Vec(IDL.Nat)],
+        [],
+      ),
+    'selection_sort' : IDL.Func([IDL.Vec(IDL.Nat)], [IDL.Vec(IDL.Nat)], []),
     'square' : IDL.Func([IDL.Nat], [IDL.Nat], []),
     'sum_of_array' : IDL.Func([IDL.Vec(IDL.Nat)], [IDL.Nat], []),
   });
